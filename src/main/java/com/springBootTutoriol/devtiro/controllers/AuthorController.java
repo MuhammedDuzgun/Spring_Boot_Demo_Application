f3594d45903c53f87a7dbfc815bc9ee4ec5975b6
muhammed <muhammedduzgun00@gmail.com>
2024-03-28 02:06:30 +0300

    Configure application.properties and implement methods in AuthorController class

--- src/main/java/com/springBootTutoriol/devtiro/controllers/AuthorController.java
+++ src/main/java/com/springBootTutoriol/devtiro/controllers/AuthorController.java
@@ -1,12 +1,33 @@
 package com.springBootTutoriol.devtiro.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springBootTutoriol.devtiro.entites.Author;
+import com.springBootTutoriol.devtiro.repositories.IAuthorRepository;
+import com.springBootTutoriol.devtiro.services.IAuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(name = "/authors")
+@RequestMapping("/authors")
 public class AuthorController {
 
+    private IAuthorService authorService;
+
+    @Autowired
+    public AuthorController(IAuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/authors")
+    public List<Author> getAllAuthors() {
+        return this.authorService.getAuthors();
+    }
+
+    @PostMapping("/add-author")
+    public void addAuthor(@RequestBody Author author) {
+        authorService.addAuthor(author);
+    }
 
 
 }
